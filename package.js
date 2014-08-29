cd722b5ec3d1b1d158d2cb4498fe64b747ac8e2a
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-08-29 19:26:16 +0200

    Remove reference to empty file

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
-  api.addFiles('urigo:angular-ui-bootstrap-calendar.js');
+  //api.addFiles('urigo:angular-ui-bootstrap-calendar.js');
   api.use('urigo:ngmeteor@0.2.0', 'client');
   // Client files.
   api.addFiles('ui-bootstrap-tpls.min.js', 'client');
