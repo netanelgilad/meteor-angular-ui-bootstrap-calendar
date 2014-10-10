d0ce630a258e68ffd7e30f1242fbe4f2bd6257d7
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-10 03:14:59 +0300

    Update to new angular-meteor version

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
   //api.addFiles('urigo:angular-ui-bootstrap-calendar.js');
-  api.use('urigo:ngmeteor@0.4.2', 'client');
+  api.use('urigo:angular@0.4.2', 'client');
   // Client files.
   api.addFiles('ui-bootstrap-tpls.min.js', 'client');
 });
