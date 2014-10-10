f1aa37138e6e5aa6acacb90b5825e5cd30aa4789
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-10 03:14:06 +0300

    Update to new angular-meteor version

--- init.js
+++
@@ -1,7 +0,0 @@
-// Add module 'ui.bootstrap' as dependency.
-Meteor.startup(function() {
-  if (typeof(ngMeteor) !== 'undefined') {
-    ngMeteor.requires.push('ui.bootstrap');
-  }
-});
-
--- package.js
+++ package.js
@@ -1,16 +1,15 @@
 Package.describe({
   summary: "Meteor package for my fork of ui-bootstrap's date picker",
-  version: "0.0.2",
+  version: "0.0.3",
   git: "https://github.com/Urigo/meteor-angular-ui-bootstrap-calendar.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
   //api.addFiles('urigo:angular-ui-bootstrap-calendar.js');
-  api.use('urigo:ngmeteor@0.2.0', 'client');
+  api.use('urigo:ngmeteor@0.4.2', 'client');
   // Client files.
   api.addFiles('ui-bootstrap-tpls.min.js', 'client');
-  api.addFiles('init.js', 'client');
 });
 
 Package.onTest(function(api) {
